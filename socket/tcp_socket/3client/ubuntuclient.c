f0a8d06862b28e127f935eeefc20bc0534748ca9
zbjoy <664266432@qq.com>
2024-02-07 20:57:13 +0800

    服务器端练习(有bug,接通后服务端无法读入数据写出)

---
+++ socket/tcp_socket/3client/ubuntuclient.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_fd == -1)
+	{
+		perror("socket");	
+		return 1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	inet_pton(AF_INET, "192.168.56.128", &addr.sin_addr.s_addr);
+
+	socklen_t addrlen = (socklen_t)sizeof(addr);
+	int ret = connect(socket_fd, (struct sockaddr*)&addr, addrlen);
+	if(ret == -1)
+	{
+		perror("connect");	
+		return 1;
+	}
+
+	char buf[1024];
+	while(1)
+	{
+		memset(buf, 0, sizeof(buf));
+		int n = read(STDIN_FILENO, buf, sizeof(buf));	
+		write(socket_fd, buf, n);
+		n = read(socket_fd, buf, sizeof(buf));
+		write(STDOUT_FILENO, buf, n);
+	}
+
+	close(socket_fd);
+
+	return 0;
+}
---
+++ socket/tcp_socket/4server/server.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock_fd == -1)
+	{
+		perror("socket");	
+		return 1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	inet_pton(AF_INET, "192.168.56.128", &addr.sin_addr.s_addr);
+	socklen_t addrlen = (socklen_t)sizeof(addr);
+	int ret = bind(sock_fd, (struct sockaddr*)&addr, addrlen);
+	if(ret == -1)
+	{
+		perror("bind");	
+		return 1;
+	}
+
+	ret = listen(sock_fd, 128);
+	if(ret == -1)
+	{
+		perror("listen");	
+		return 1;
+	}
+
+	int fd = accept(sock_fd, (struct sockaddr*)&addr, &addrlen);
+	printf("连接成功.....\n");
+	while(1)
+	{
+		printf("you can get client information......\n");
+		char buf[1024] = "";
+		int n = read(sock_fd, buf, sizeof(buf));	
+		if(n == 0)
+		{
+			printf("no read.....\n");		
+		}
+		printf("read.....\n");
+		write(STDOUT_FILENO, buf, n);
+		printf("write stdout......\n");
+	//	n = read(STDIN_FILENO, buf, sizeof(buf));
+		char hello[] = "i listen to you.....\n";
+		int num = read(STDIN_FILENO, buf, sizeof(buf));
+		write(sock_fd, buf, num);
+		//write(sock_fd, hello, sizeof(hello));
+	}
+
+	return 0;
+}
