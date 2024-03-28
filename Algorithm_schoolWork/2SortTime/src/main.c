547e54ed23d0112510cb5fc6e6b47d30efd8a819
zbjoy <664266432@qq.com>
2024-03-28 23:39:59 +0800

    众数问题的练习

--- Algorithm_schoolWork/2SortTime/src/main.c
+++ Algorithm_schoolWork/2SortTime/src/main.c
@@ -5,6 +5,42 @@
 #include "BubbleSort.h"
 #include "MergeSort.h"
 
+int count_element(int* nums, int start, int end, int target)
+{
+    int count = 0;
+    for (int i = start; i <= end; i++)
+    {
+        if (nums[i] == target)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int mode_recursive(int* nums, int start, int end)
+{
+    if (start == end)
+    {
+        return nums[start];
+    }
+
+    if (end - start == 1)
+    {
+        return (count_element(nums, start, end, nums[start]) >= count_element(nums, start, end, nums[end])) ? nums[start] : nums[end];
+    }
+
+    int mid = (start + end) / 2;
+    int left_mode = mode_recursive(nums, start, mid);
+    int right_mode = mode_recursive(nums, mid + 1, end);
+
+    // 比较左右众数在整体数组中的出现次数
+    int left_count = count_element(nums, start, end, left_mode);
+    int right_count = count_element(nums, start, end, right_mode);
+
+    // 返回出现次数最多的众数
+    return (left_count >= right_count) ? left_mode : right_mode;
+}
 
 int main(int argc, char** argv)
 {
@@ -23,7 +59,10 @@ int main(int argc, char** argv)
     printf("BubbleSort: ");
     print_sorttime(BubbleSort, nums, n);
     printf("MergeSort: ");
-    print_sorttime(MergeSort, nums, n);
+    /* print_sorttime(MergeSort, nums, n); */
+    get_sorttime(MergeSort, nums, n);
+
+    printf("the mode is %d\n", mode_recursive(nums, 0, n - 1));
 
     getdata_delete(&nums);
 
